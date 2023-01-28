d3b35340d32322630fe60845d06d35ffb3124e79
Fetyali7 <fetyali7@gmail.com>
2023-01-28 10:02:37 -0500

    Client Class

---
+++ Client.java
@@ -0,0 +1,40 @@
+class Client extends Thread{
+
+    int numberOfTransactions;
+    int maxNbTransactions;
+    Transactions[] transcations;
+    String clientOperation;
+    Network onjNetwork;
+
+    public int getNumberOfTransacrions(){
+
+    }
+    public String getClientOperation(){
+
+    }
+    public void setNumberOfTransactions(int nbOfTrans){
+
+    }
+    public void setClientOperation(String operation){
+
+    }
+    public void readTransactions(){
+
+    }
+    public void sendTransactions(){
+
+    }
+    public void receiveTransactions(Transactions transact){
+
+    }
+    public String toString(){
+
+    }
+    public void run(){
+
+    }
+
+
+
+
+}
