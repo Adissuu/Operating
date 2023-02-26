c2c8498a331c9711cbf88a902c7cf340a1b5a961
Gabriel <gabrieldubois.eng@gmail.com>
2023-02-26 11:23:15 -0500

    formating the repository into separate assignments

---
+++ A1/Driver.java
@@ -0,0 +1,34 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @authors Ali Fetanat (40158208), Gabriel Dubois (40209252), Kerly Titus
+ */
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	
+    	 /*******************************************************************************************************************************************
+    	  * TODO : implement all the operations of main class   																					*
+    	  ******************************************************************************************************************************************/
+        
+    	Network objNetwork = new Network("network");           /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server();                    /* Initializing the server */
+        objServer.start();  
+        Client objsender = new Client("sending");
+        objsender.start();                              
+        Client objreceiver = new Client("receiving");
+        objreceiver.start();
+        
+    }
+}
